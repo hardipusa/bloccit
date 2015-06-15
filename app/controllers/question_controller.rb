9696e4f96f3ee80f67ebd359905302be102479fd
Hardip Singh <Hardip.singh.ca@gmail.com>
2015-06-15 03:18:03 -0500

    assigment-35-morecrud-almostcompleted

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,21 @@
+class QuestionController < ApplicationController
+ 
+  def index
+  end
+
+  def individual
+  end
+
+  def new
+  end
+
+  def edit
+  end 
+
+  def update 
+  end
+
+  def delete 
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :posts
 
+resources :questions, :index, :individual, :new, :edit, :update, :delete
   get 'about' => 'welcome#about'
   
   root to: 'welcome#index'
