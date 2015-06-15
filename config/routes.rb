ab131cc10ac694788c6a764786f6a3e29066ac13
Hardip Singh <Hardip.singh.ca@gmail.com>
2015-06-15 09:32:13 -0500

    updatedQuestionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
+
   resources :posts
 
 resources :questions, :index, :individual, :new, :edit, :update, :delete
-=======
-   resources :posts
-   
->>>>>>> 27bbed284cf37bb03233fa209317c403ec92934b
+
   get 'about' => 'welcome#about'
   
   root to: 'welcome#index'
